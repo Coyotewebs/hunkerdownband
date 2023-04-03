7a98f6eee237b8bc6b46b27c05a615abe3f4c32c
Coyotewebs <88667536+Coyotewebs@users.noreply.github.com>
2023-04-03 13:16:44 -0600

    first commit

---
+++ backendhunkerdownapp/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'kvmez53x',
+    dataset: 'production'
+  }
+})
---
+++ backendhunkerdownapp/schemas/index.ts
@@ -0,0 +1,9 @@
+
+import category from './category';
+import banner from './banner';
+import product from './product';
+import news from './news';
+
+
+
+export const schemaTypes = [category, banner, product, news ]
